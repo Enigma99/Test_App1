d48b841e5c71091772af7c87998fa688e2c02ada
Enigma James <enigma_1m2@yahoo.com>
2017-12-02 21:19:37 -0500

    http server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// http.createServer((req, res) => {
+//
+// });
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.write("Hello Node!!\n");
+
+  setTimeout(() => {
+    res.write("Still on....");
+    res.end();
+  }, 3000)
+
+});
+
+server.listen(8080);
