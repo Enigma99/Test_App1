891791bedbe780d48b20da307660ea5631d9b55c
Enigma James <enigma_1m2@yahoo.com>
2017-12-03 22:45:34 -0500

    add heroku db

--- server/db.js
+++ server/db.js
@@ -23,7 +23,11 @@ var pg = require('pg');
 // });
 
 var pool = new pg.Pool({
-  database: 'books-dev'
+  // database: 'books-dev' // localdb,
+  database: 'd2ceiis1l0nvhm',
+  user: 'nfmxxrdcpvnpqv',
+  password: '2ab10e6a3d0f75a236e499914333ca19d0d7b8af26e347093fbd82e04bed5d3c',
+  host: 'ec2-107-20-188-239.compute-1.amazonaws.com',
 });
 
   pool.query('SELECT * from books', [], function(err, result){
