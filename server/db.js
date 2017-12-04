ba5e798866ac3450e23064e2f9b3b6740519cbb8
Enigma James <enigma_1m2@yahoo.com>
2017-12-03 21:20:21 -0500

    add db.js and updated package.json file to include nodemon script

---
+++ server/db.js
@@ -0,0 +1,23 @@
+var pg = require('pg');
+
+var client = new pg.Client({
+  database: 'books-dev'
+});
+
+//connect to db
+client.connect(function(err){
+  if (err) throw err;
+
+  //execut a query
+  client.query('SELECT * from books', [], function(err, result){
+    if (err) throw err;
+
+    //just print the results to the console
+    console.log(result.rows);
+
+    //disconnect the Client
+    client.end(function(err){
+      if (err) throw err;
+    });
+  });
+});
